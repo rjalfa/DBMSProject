5e6c0343af4f17f1f5d34c99e55267ef572d978a
Rounaq <rounaqwl66@gmail.com>
2016-04-06 11:30:21 +0530

    Interop

--- src/com/iiitd/dbms/medsh/Main.java
+++ src/com/iiitd/dbms/medsh/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
 			loader.setLocation(Main.class.getResource("view/LoginPage.fxml"));
 			BorderPane root = (BorderPane) loader.load();
 			Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
