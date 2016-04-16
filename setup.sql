90dc950b8a2ab05a48dd438dbbb66d4e26089651
Rounaq <rounaqwl66@gmail.com>
2016-04-16 09:34:44 +0530

    Added EmployeeRecord. Need to refactor. Changes to Admin Interface

---
+++ setup.sql
@@ -0,0 +1,5 @@
+create database DBMS_test;
+use DBMS_test;
+
+create table Employee (uid bigint PRIMARY KEY, name varchar(100), dob date, gender char(1), contact varchar(20), doj date, payroll double(18,2),username varchar(20), password varchar(32));
+
--- src/com/iiitd/dbms/medsh/record/EmployeeRecord.java
+++ src/com/iiitd/dbms/medsh/record/EmployeeRecord.java
@@ -9,7 +9,7 @@ import com.iiitd.dbms.medsh.model.Employee;
 
 public class EmployeeRecord {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/<<database_name>>";
+	static final String DB_URL = "jdbc:mysql://localhost/DBMS_test";
 	static final String DB_USER = "root";
 	static final String DB_PASS = "rounaq";
 	private Employee.EscalateAccess accessPassKey;
@@ -49,16 +49,15 @@ public class EmployeeRecord {
 		{
 			rowSet.moveToInsertRow();
 			rowSet.updateLong("uid", e.getUid());
-			rowSet.updateLong("", e.getUid());
-			rowSet.updateString("", e.getName());
-			rowSet.updateDate("", new java.sql.Date(e.getDateOfBirth().getTime()));
-			rowSet.updateString("", e.getGender());
-			rowSet.updateString("", e.getContact());
-			rowSet.updateDate("", new java.sql.Date(e.getDateOfJoining().getTime()));
-			rowSet.updateDouble("", e.getPayroll());
-			rowSet.updateString("", e.getUserName());
+			rowSet.updateString("name", e.getName());
+			rowSet.updateDate("dob", new java.sql.Date(e.getDateOfBirth().getTime()));
+			rowSet.updateString("gender", e.getGender());
+			rowSet.updateString("contact", e.getContact());
+			rowSet.updateDate("doj", new java.sql.Date(e.getDateOfJoining().getTime()));
+			rowSet.updateDouble("payroll", e.getPayroll());
+			rowSet.updateString("username", e.getUserName());
 			e.importAccessKey(this);
-			rowSet.updateString("",accessPassKey.getPassword());
+			rowSet.updateString("password",accessPassKey.getPassword());
 		}
 		catch(SQLException ex)
 		{
@@ -72,16 +71,15 @@ public class EmployeeRecord {
 		try
 		{
 			rowSet.updateLong("uid", e.getUid());
-			rowSet.updateLong("", e.getUid());
-			rowSet.updateString("", e.getName());
-			rowSet.updateDate("", new java.sql.Date(e.getDateOfBirth().getTime()));
-			rowSet.updateString("", e.getGender());
-			rowSet.updateString("", e.getContact());
-			rowSet.updateDate("", new java.sql.Date(e.getDateOfJoining().getTime()));
-			rowSet.updateDouble("", e.getPayroll());
-			rowSet.updateString("", e.getUserName());
+			rowSet.updateString("name", e.getName());
+			rowSet.updateDate("dob", new java.sql.Date(e.getDateOfBirth().getTime()));
+			rowSet.updateString("gender", e.getGender());
+			rowSet.updateString("contact", e.getContact());
+			rowSet.updateDate("doj", new java.sql.Date(e.getDateOfJoining().getTime()));
+			rowSet.updateDouble("payroll", e.getPayroll());
+			rowSet.updateString("username", e.getUserName());
 			e.importAccessKey(this);
-			rowSet.updateString("",accessPassKey.getPassword());
+			rowSet.updateString("password",accessPassKey.getPassword());
 			rowSet.updateRow();
 			rowSet.moveToCurrentRow();
 		}
@@ -120,4 +118,72 @@ public class EmployeeRecord {
 			ex.printStackTrace();
 		}
 	}
+	
+	public Employee first()
+	{
+		Employee e = new Employee();
+		try
+		{
+			rowSet.first();
+			populateData(e);
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+		return e;
+	}
+	
+	public Employee last()
+	{
+		Employee e = new Employee();
+		try
+		{
+			rowSet.last();
+			populateData(e);
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+		return e;
+	}
+	
+	public Employee find(long uid)
+	{
+		Employee e = new Employee();
+		try
+		{
+			rowSet.last();
+			populateData(e);
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+		return e;
+	}
+	
+	private Employee populateData(Employee e)
+	{
+		try
+		{
+			e.setUid(rowSet.getLong("uid"));
+			e.setName(rowSet.getString("name"));
+			e.setDateOfBirth(rowSet.getDate("dob"));
+			e.setGender(rowSet.getString("gender"));
+			e.setContact(rowSet.getString("contact"));
+			e.setDateOfJoining(rowSet.getDate("doj"));
+			e.setPayroll(rowSet.getDouble("payroll"));
+			e.setUserName(rowSet.getString("username"));
+			e.setPassword(rowSet.getString("password"));
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+		return e;
+	}
+	
+	
 }
