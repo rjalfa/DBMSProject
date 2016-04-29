14b9556d46390a6c933841ee8a43941184a20041
Rounaq <rounaqwl66@gmail.com>
2016-04-29 07:09:58 +0530

    Added Accounts Interface[incomplete] and LoginDetails Class

---
+++ src/com/iiitd/dbms/medsh/model/LoginDetails.java
@@ -0,0 +1,19 @@
+package com.iiitd.dbms.medsh.model;
+
+public class LoginDetails {
+	private long uid;
+	private String userName;
+	public long getUid() {
+		return uid;
+	}
+	public void setUid(long uid) {
+		this.uid = uid;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
